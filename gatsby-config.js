cf3d6b6b73212c47c177a62d6a81ab1efa736acf
nodox <snatera09@gmail.com>
2018-07-27 23:55:06 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+let contentfulConfig
+
+try {
+  contentfulConfig = require('./.contentful')
+} catch (_) {
+  contentfulConfig = {
+    spaceId: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
+  }
+} finally {
+  const { spaceId, accessToken } = contentfulConfig
+
+  if (!spaceId || !accessToken) {
+    throw new Error(
+      'Contentful spaceId and the delivery token need to be provided.'
+    )
+  }
+}
+
+module.exports = {
+  pathPrefix: process.env.CI ? `/identity` : `/`,
+  siteMetadata: {
+    author: 'You!',
+    title: `Gatsby Default (Blog) Starter`,
+  },
+  plugins: [
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-catch-links',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: contentfulConfig,
+    },
+  ],
+}
