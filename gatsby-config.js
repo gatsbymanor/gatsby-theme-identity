409ab7331d6a8464c7f34dbf25929aad6881a643
nodox <snatera09@gmail.com>
2018-09-03 13:21:59 -0400

    upgrade to v2 starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,20 @@
 var gtk = require('gatsby-themes-kit')
 
 var sitePlugins = [
-  'gatsby-plugin-react-next',
   'gatsby-plugin-catch-links',
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: 'gatsby-starter-default',
+      short_name: 'starter',
+      start_url: '/',
+      background_color: '#663399',
+      theme_color: '#663399',
+      display: 'minimal-ui',
+      icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+    },
+  },
+  'gatsby-plugin-offline',
 ]
 
 gtk.loadPlugins().forEach(plugin => {
